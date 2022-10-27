2380f7796f33a34f4bb7ade36a8cf5be60c8d7a6
Vitaliy <vit.ivoylov@yandex.ru>
2022-10-27 15:50:51 +0500

    Refactor

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,4 +1,6 @@
-import java.net.URI;import java.util.Scanner;import java.io.IOException;
+import java.net.URI;
+import java.util.Scanner;
+import java.io.IOException;
 
 public class calculate_deposit
 {
